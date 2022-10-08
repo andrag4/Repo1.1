f9b1ae8b551b46c4d642eaec4d6fe9d2e6651f2f
Andrey Lukin <113754757+andrag4@users.noreply.github.com>
2022-10-08 11:27:14 +0700

    Add files via upload

---
+++ Repo1/Проверка на високосный год/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 0;
+            string str;
+            Console.WriteLine(" Напишите год ");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            if ((a % 4 == 0) & (a % 100 != 0) | (a % 400 == 0))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" Високосный год");
+            }
+            else
+            {
+                Console.WriteLine(" Не високосный год");
+            }
+        }
+    }
+}
